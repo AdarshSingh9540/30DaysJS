185b46d13291d4811a82a4f37a17a28923d5e5ea
Adarsh <adarsh9540984202@gmail.com>
2024-07-14 21:20:10 +0530

    Day 2

---
+++ javascript/day2.js
@@ -0,0 +1,70 @@
+// function add(a,b){
+//     return a+b;
+// }
+
+// console.log(add(2,3))
+
+
+// function sub(a,b){
+//     return a-b;
+// }
+
+// console.log(sub(2,3))
+
+
+// function mul(a,b){
+//     return a*b;
+// }
+
+// console.log(mul(2,3))
+
+
+// function div(a,b){
+//     if(b==0) return "Invalid outpt";
+//     return (a/b).toFixed(2)
+// }
+
+// console.log(div(2,3))
+
+
+// function div(a,b){
+//     if(b==0) return "Invalid outpt";
+//     return (a%b)
+// }
+
+// console.log(div(23,4))
+
+
+// console.log(4==4)
+
+// function myfun(age,city){
+//     if(age>=18 && city==="delhi"){
+//         return "You are Eligible for Staying in delhi"
+// }
+//   return "You are not eligible"
+// }
+
+
+// console.log(myfun(16,"delhi"));
+// console.log(myfun(25,"delhi"))
+// console.log(myfun(85,"Mumbai"))
+
+
+// function myfun(age,city){
+//     if(age>=18 || city==="delhi"){
+//         return "You are Eligible for Staying in delhi"
+// }
+//   return "You are not eligible"
+// }
+
+
+// console.log(myfun(16,"Delhi"));
+// console.log(myfun(25,"delhi"))
+// console.log(myfun(85,"Mumbai"))
+
+function myfun(age){
+    return age>=18 ? "Senoir" :"junior";
+}
+
+console.log(myfun(23))
+console.log(myfun(13))
