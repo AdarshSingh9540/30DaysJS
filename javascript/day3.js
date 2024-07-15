a7675af1a229d80f6e94366beda8e5e3bd07869f
Adarsh <adarsh9540984202@gmail.com>
2024-07-15 13:15:16 +0530

    Day 3

---
+++ javascript/day3.js
@@ -0,0 +1,84 @@
+// function myfun(num){
+//     if(num>0){
+//         if(num%2==0){
+//             console.log("Even Number")
+//         }else{
+//               console.log("Not a even number")
+//             }
+//     }else {
+//         console.log("Negative Number")
+//     }
+// }
+
+// myfun(-858)
+
+
+// function myfun(x, y, z) {
+//     if (x > y && x > z) {
+//         console.log("X is greatest among all of them");
+//     } else if (y > x && y > z) {
+//         console.log("Y is greatest among all of them");
+//     } else {
+//         console.log("Z is greatest!");
+//     }
+// }
+
+
+// myfun(255,85,96)
+
+
+// function myfun(dayNo){
+//     let dayname;
+//     switch(dayNo){
+//         case 0:
+//             dayname = "Sunday";
+//             break;
+
+//         case 1:
+//             dayname="Monday";
+//             break;
+         
+//         case 2:
+//             dayname="Tuesday";
+//             break;
+//         case 3:
+//             dayname="Wednesday";
+//             break;
+//         case 4 :
+//             dayname="Thursday";
+//             break;
+//         case 5 :
+//             dayname="friday"
+//             break;
+//          case 6:
+//             dayname="saturday";
+//             break;
+//          default:
+//             dayname="Invalid day"    
+//             break;
+//     }
+
+//     return dayname;
+// }
+
+// console.log(myfun(6))
+
+
+// function myfun(num){
+//     return num%2==0 ? "even":"odd";
+// }
+
+// console.log(myfun(81))
+
+
+
+// function myfun(year) {
+//     if ((year % 4 === 0 && year % 100 !== 0) || (year % 400 === 0)) {
+//         console.log(year + " is a leap year");
+//     } else {
+//         console.log(year + " is not a leap year");
+//     }
+// }
+
+
+// myfun(2024)
