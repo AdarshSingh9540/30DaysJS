a24e4bed8067349744a604cf1324e91f014ce7d9
Adarsh <adarsh9540984202@gmail.com>
2024-07-18 09:17:53 +0530

    update day 6

---
+++ javascript/day6.js
@@ -0,0 +1,75 @@
+// const arr = [1,2,3,4,5];
+// console.log(arr)
+// console.log(arr[0]+","+arr[arr.length-1])
+
+
+// arr.push(6)
+// console.log(arr)
+// arr.pop()
+// console.log(arr)
+// arr.shift()
+// console.log(arr)
+// arr.unshift(1)
+// console.log(arr)
+
+
+// const arr = [1, 2, 3, 4, 5];
+
+// const newArr =arr.map((e) => e * 2)
+// console.log(newArr.toString())
+
+
+// const filtRes = newArr.filter(e => e%3===0);
+// console.log(filtRes)
+
+// const doubleRes =arr.map((e) => e * 2);
+// const sumOfDouble = doubleRes.reduce((accumulator,currentValue) => accumulator+currentValue,0);
+// console.log(sumOfDouble)
+
+
+// const arr = [1, 2, 3, 4, 5];
+
+// for(let i=0 ;i<arr.length;i++){
+//     console.log(arr[i])
+// }
+
+
+// arr.forEach((x)=>{
+//     console.log(x*2)
+// })
+
+const arr = [
+    [1, 2, 3],
+    [4, 5, 6],
+    [7, 8, 9]
+  ];
+
+//   arr.forEach((x)=>{
+//     console.log(x)
+//   })
+
+//   arr.forEach((x)=>{
+//     x.forEach((y)=>{
+//         console.log(y)
+//     })
+//   })
+
+// for(let i=0;i<arr.length;i++){
+//     for(let j=0;j<arr.length;j++){
+//         console.log(arr[i][j]);
+//     }
+// }
+
+
+// for(let i=0;i<arr.length;i++){
+//     for(let j=0;j<arr.length;j++){
+//         console.log(arr[i][j]);
+//     }
+// }
+
+console.log(arr[2][2]);
+console.log(arr[1][1]);
+console.log(arr[0][0]);
+
+
+
